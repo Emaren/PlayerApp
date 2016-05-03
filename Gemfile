5c540bea205f0479d4b63dcaef71cb6aac82c51a
Tony Blum <tonyblum@me.com>
2016-05-02 18:07:55 -0700

    create form and complete player controller

--- Gemfile
+++ Gemfile
@@ -23,14 +23,9 @@ gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
 
-# Use ActiveModel has_secure_password
-# gem 'bcrypt', '~> 3.1.7'
+# Step 1) add simple form gem, then bundle
+gem 'simple_form'
 
-# Use Unicorn as the app server
-# gem 'unicorn'
-
-# Use Capistrano for deployment
-# gem 'capistrano-rails', group: :development
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
@@ -44,4 +39,3 @@ group :development do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
 end
-
--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,16 +1,53 @@
 class PlayersController < ApplicationController
-  def index
+
+  # New player
+  def new
+    @player = Player.new
   end
 
+  # Create new player in the DB
   def create
+    @player = Player.new player_params
+    if @player.save
+      redirect_to player_path(@player), notice: "Player Successfully created!"
+    else
+      flash[:alert] = "See error(s) below..."
+      render :new
+    end
+
   end
 
+  # Index all Player
+  def index
+    @players = Play.all
+  end
+
+  # Display each player
   def show
+    @player = Player.find params[:id]
   end
 
+  # Update a player's info
   def update
+    @player = Player.find params[:id]
+    if @player.update player_params
+      redirect_to player_path(@player), notice: "Player Successfully updated!"
+    else
+      flash[:alert] = "See error(s) below..."
+      render :edit
+    end
   end
 
+  # Delete player record from the DB.
   def destroy
+    @player = player.find params[:id]
+    @player.destory
+    redirect_to players_path
+  end
+
+  private
+
+  def player_params
+    params.require(:player).permit(:name, :age, :team)
   end
 end
